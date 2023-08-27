f231e8c9977398dde551b9f61316099334f9a53c
Chan <87248999+Chan5k@users.noreply.github.com>
2023-08-27 15:11:16 +0300

    Update swfs.sh

--- swfs.sh
+++ swfs.sh
@@ -1,20 +1,16 @@
 #!/bin/bash
 
-NGINX_PORT=8080
-APACHE_PORT=8081
-
 function check_python() {
     if command -v python3 &> /dev/null; then
         echo -e "\e[92mPython3 is installed.\e[0m"
     else
         echo -e "\e[91mPython3 is not installed. Please install Python3 to use this script.\e[0m"
-        echo "You can install Python using your package manager. For example, on Arch Linux:"
-        echo "sudo pacman -S python"
+        echo "You can download and install Python from https://www.python.org/downloads/"
         exit 1
     fi
 }
 
-function start_python_server() {
+function start_file_server() {
     echo "Enter the directory to serve files from:"
     read -r directory
 
@@ -26,7 +22,23 @@ function start_python_server() {
     echo "Enter the port number:"
     read -r port
 
-    python3 -m http.server "$port" --directory "$directory"
+    if ! [[ "$port" =~ ^[0-9]+$ ]]; then
+        echo -e "\e[91mError: Port number must be a valid integer.\e[0m"
+        return
+    fi
+
+    echo "Start the file server in the background? (y/n):"
+    read -r background_choice
+
+    if [ "$background_choice" = "y" ] || [ "$background_choice" = "Y" ]; then
+        echo -e "\e[92mStarting file server on port $port in the background...\e[0m"
+        python3 -m http.server "$port" --directory "$directory" > /dev/null 2>&1 &
+        server_pid=$!  # Get the PID of the background process
+        echo "File server is running in the background. Use 'kill $server_pid' to end it."
+    else
+        echo -e "\e[92mStarting file server on port $port...\e[0m"
+        python3 -m http.server "$port" --directory "$directory"
+    fi
 }
 
 clear  # Clear the console before displaying the menu
@@ -34,15 +46,14 @@ clear  # Clear the console before displaying the menu
 check_python
 
 echo -e "\e[96mWelcome to the Professional File Server Script!\e[0m"
-echo "Choose a server type:"
-echo "1. Python built-in server"
+echo "1. Start File Server"
 echo "2. Quit"
 
 read -r choice
 
 case $choice in
     1)
-        start_python_server
+        start_file_server
         ;;
     2)
         echo "Exiting..."
@@ -51,5 +62,3 @@ case $choice in
         echo "Invalid choice"
         ;;
 esac
-
-exit 0
