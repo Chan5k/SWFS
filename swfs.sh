e608ec8ab0038bb27a67bc9f47f3c010690a051f
Chan <87248999+Chan5k@users.noreply.github.com>
2023-08-27 14:57:39 +0300

    Update swfs.sh

--- swfs.sh
+++ swfs.sh
@@ -5,12 +5,13 @@ function check_python() {
         echo -e "\e[92mPython3 is installed. Script loading..\e[0m"
     else
         echo -e "\e[91mPython3 is not installed. Please install Python3 to use this script.\e[0m"
-        echo "You can download and install Python from https://www.python.org/downloads/"
+        echo "You can install Python using your package manager. For example, on Arch Linux:"
+        echo "sudo pacman -S python"
         exit 1
     fi
 }
 
-function start_file_server() {
+function start_python_server() {
     echo "Enter the directory to serve files from:"
     read -r directory
 
@@ -22,44 +23,50 @@ function start_file_server() {
     echo "Enter the port number:"
     read -r port
 
-    if ! [[ "$port" =~ ^[0-9]+$ ]]; then
-        echo -e "\e[91mError: Port number must be a valid integer.\e[0m"
-        return
-    fi
+    python3 -m http.server "$port" --directory "$directory"
+}
 
-    echo "Start the file server in the background? (y/n):"
-    read -r background_choice
+function start_nginx_server() {
+    sudo pacman -S nginx
+    sudo systemctl start nginx
+    echo -e "\e[92mNginx server started. Access your files at http://localhost.\e[0m"
+}
 
-    if [ "$background_choice" = "y" ] || [ "$background_choice" = "Y" ]; then
-        echo -e "\e[92mStarting file server on port $port in the background...\e[0m"
-        python3 -m http.server "$port" --directory "$directory" > /dev/null 2>&1 &
-        server_pid=$!  # Get the PID of the background process
-        echo "File server is running in the background. Use 'kill $server_pid' to end it."
-    else
-        echo -e "\e[92mStarting file server on port $port...\e[0m"
-        python3 -m http.server "$port" --directory "$directory"
-    fi
+function start_apache_server() {
+    sudo pacman -S apache
+    sudo systemctl start httpd
+    echo -e "\e[92mApache server started. Access your files at http://localhost.\e[0m"
 }
 
 clear  # Clear the console before displaying the menu
 
 check_python
 
-echo -e "\e[96mWelcome to SWFS\e[0m"
-echo "1. Start File Server"
-echo "2. Quit"
+echo -e "\e[96mWelcome to SWFS!\e[0m"
+echo "Choose a server type:"
+echo "1. Python built-in server"
+echo "2. Nginx server"
+echo "3. Apache server"
+echo "4. Quit"
 
 read -r choice
 
 case $choice in
     1)
-        start_file_server
+        start_python_server
         ;;
     2)
+        start_nginx_server
+        ;;
+    3)
+        start_apache_server
+        ;;
+    4)
         echo "Exiting..."
         ;;
     *)
         echo "Invalid choice"
         ;;
 esac
+
 exit 0
